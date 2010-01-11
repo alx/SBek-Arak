fa70cdc278bdda7d683ee4af5b5523ec425b6704
Alexandre Girard <alx.girard@gmail.com>
2010-01-11 23:19:59 +0100

    add sbekarak theme

---
+++ wp-content/themes/sbekarak/index.php
@@ -0,0 +1,15 @@
+<div class="entete1" align="center"><img src="<?php get_template_directory_uri(); ?>/images/index_headtop.gif" /></div> 
+<div class="entete" align="center">SBek Arak</div> 
+<div class="entete2" align="center"><img src="<?php get_template_directory_uri(); ?>/images/index_headbottom.gif" /></div> 
+ 
+ 
+ 
+<div class="footer1"><img src="<?php get_template_directory_uri(); ?>/images/index_foottop.gif" /><div> 
+<div class="footer a"> 
+<table align="center" cellpadding="5"> 
+<tr> 
+<td>En construction...</td>
+</tr> 
+</table> 
+</div> 
+<div class="footer2"><img src="<?php get_template_directory_uri(); ?>/images/index_footbottom.gif" /></div>
