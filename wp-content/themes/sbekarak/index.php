db35c94b458c0f630c175b319b39d055be12a3fa
Alexandre Girard <alx.girard@gmail.com>
2010-01-11 23:25:25 +0100

    add header/footer

--- wp-content/themes/sbekarak/index.php
+++ wp-content/themes/sbekarak/index.php
@@ -1,3 +1,5 @@
+<?php get_header(); ?>
+
 <div class="entete1" align="center"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_headtop.gif" /></div> 
 <div class="entete" align="center">SBek Arak</div> 
 <div class="entete2" align="center"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_headbottom.gif" /></div> 
@@ -13,3 +15,5 @@
 </table> 
 </div> 
 <div class="footer2"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_footbottom.gif" /></div>
+
+<?php get_footer(); ?>
