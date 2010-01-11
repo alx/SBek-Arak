aba7adaba8e4065d53bb43a61d33fa044bda10c3
Alexandre Girard <alx.girard@gmail.com>
2010-01-11 23:23:08 +0100

    correct images path

--- wp-content/themes/sbekarak/index.php
+++ wp-content/themes/sbekarak/index.php
@@ -1,10 +1,10 @@
-<div class="entete1" align="center"><img src="<?php get_template_directory_uri(); ?>/images/index_headtop.gif" /></div> 
+<div class="entete1" align="center"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_headtop.gif" /></div> 
 <div class="entete" align="center">SBek Arak</div> 
-<div class="entete2" align="center"><img src="<?php get_template_directory_uri(); ?>/images/index_headbottom.gif" /></div> 
+<div class="entete2" align="center"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_headbottom.gif" /></div> 
  
  
  
-<div class="footer1"><img src="<?php get_template_directory_uri(); ?>/images/index_foottop.gif" /><div> 
+<div class="footer1"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_foottop.gif" /><div> 
 <div class="footer a"> 
 <table align="center" cellpadding="5"> 
 <tr> 
@@ -12,4 +12,4 @@
 </tr> 
 </table> 
 </div> 
-<div class="footer2"><img src="<?php get_template_directory_uri(); ?>/images/index_footbottom.gif" /></div>
+<div class="footer2"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_footbottom.gif" /></div>
