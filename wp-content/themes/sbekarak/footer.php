6405a6885419da24f739fe4735061aa2c5d3ee9c
Alexandre Girard <alx.girard@gmail.com>
2010-02-25 13:41:58 +0100

    rename sbekarak theme

--- wp-content/themes/sbekarak/footer.php
+++ wp-content/themes/sbekarak/footer.php
@@ -1,4 +1,22 @@
-<?php wp_footer(); ?>
+<!-- begin footer -->
+
+<div style="clear:both;"></div>
+
 </div>
+
+
+                
+<div id="footer">
+
+    <div class="footer">
+		<a href="catalogue.html">English</a>
+		<a href="fr_catalogue.html">Fran&ccedil;ais</a>
+		<a href="jp_catalogue.html">Japanese</a>
+    </div>
+    
+</div>
+                                    
+<?php do_action('wp_footer'); ?>
+
 </body>
 </html>
--- wp-content/themes/sbekarak/index.php
+++ wp-content/themes/sbekarak/index.php
@@ -1,19 +1,13 @@
 <?php get_header(); ?>
 
-<div class="entete1" align="center"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_headtop.gif" /></div> 
-<div class="entete" align="center">SBek Arak</div> 
-<div class="entete2" align="center"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_headbottom.gif" /></div> 
- 
- 
- 
-<div class="footer1"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_foottop.gif" /><div> 
-<div class="footer a"> 
-<table align="center" cellpadding="5"> 
-<tr> 
-<td style="color:#FFF;">En construction...</td>
-</tr> 
-</table> 
-</div> 
-<div class="footer2"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/index_footbottom.gif" /></div>
+<div id="content">
+	<div id="contentleft">
+		<div class="postarea">
+			<?php echo nzshpcrt_display_categories_groups(); ?>
+        </div>
+	</div>
+</div>
+
+<!-- The main column ends  -->
 
 <?php get_footer(); ?>
