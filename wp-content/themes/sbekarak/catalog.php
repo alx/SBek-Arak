1a6afe8ae68040c9e3e09a4bff10ebdaa8cc842f
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 15:32:56 +0200

    try catalog lis display

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -2,13 +2,28 @@
 /*
 Template Name: Catalog
 */
-get_header(); ?>
+get_header(); 
+
+global $wpsc_query, $wpdb;
+$image_width = get_option('product_image_width');
+$image_height = get_option('product_image_height');
+?>
+
+?>
 
 <div id="content">
 
 	<div id="contentleft">
 	    
-		<?php echo nzshpcrt_display_categories_groups(); ?>
+	    <div class='wpsc_categories wpsc_category_grid'>
+			<?php wpsc_start_category_query(array('category_group'=> get_option('wpsc_default_category'), 'show_thumbnails'=> 1)); ?>
+				<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item" title='<?php wpsc_print_category_name();?>'>
+					<?php wpsc_print_category_image(45, 45); ?>
+				</a>
+				<?php wpsc_print_subcategory("", ""); ?>
+			<?php wpsc_end_category_query(); ?>
+			<div class='clear_category_group'></div>
+		</div>
 		
 	</div>
 
