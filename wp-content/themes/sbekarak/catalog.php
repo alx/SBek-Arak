a5268f2a389c299e789a2cdbc36667c514940dd5
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 15:54:59 +0200

    try with static

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -9,15 +9,19 @@ get_header();
 
 	<div id="contentleft">
 	    
-	    <div class='wpsc_categories wpsc_category_grid'>
-			<?php wpsc_start_category_query(array('category_group'=> get_option('wpsc_default_category'), 'show_thumbnails'=> 1)); ?>
-				<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item" title='<?php wpsc_print_category_name();?>'>
-					<?php wpsc_print_category_image(170, 170); ?>
-				</a>
-				<?php wpsc_print_subcategory("", ""); ?>
-			<?php wpsc_end_category_query(); ?>
-			<div class='clear_category_group'></div>
-		</div>
+	<div class="wpsc_categories wpsc_category_grid">
+            									<a href="http://sbekarak.alexgirard.com/products-page/au-quotidien/" class="wpsc_category_grid_item" title="Au Quotidien">
+            						<img src="index.php?wpsc_request_image=true&amp;category_id=7&amp;width=170&amp;height=170" alt="Au Quotidien" title="Au Quotidien" class="wpsc_category_image">						<p>Au Quotidien</p>
+            					</a>
+            														<a href="http://sbekarak.alexgirard.com/products-page/mini/" class="wpsc_category_grid_item" title="Mini">
+            						<img src="index.php?wpsc_request_image=true&amp;category_id=9&amp;width=170&amp;height=170" alt="Mini" title="Mini" class="wpsc_category_image">						<p>Mini</p>
+            					</a>
+            														<a href="http://sbekarak.alexgirard.com/products-page/soiree/" class="wpsc_category_grid_item" title="Soirée">
+            						<img src="index.php?wpsc_request_image=true&amp;category_id=8&amp;width=170&amp;height=170" alt="Soirée" title="Soirée" class="wpsc_category_image">						<p>Soirée</p>
+            					</a>
+            													<div class="clear_category_group"></div>
+            			</div>
+			
 		
 	</div>
 
