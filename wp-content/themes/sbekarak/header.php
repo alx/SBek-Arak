f7e535ae46756e04476473408dff0bf982219356
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 18:53:31 +0200

    remove element stack

--- wp-content/themes/sbekarak/header.php
+++ wp-content/themes/sbekarak/header.php
@@ -31,7 +31,6 @@ sfHover = function() {
 }
 if (window.attachEvent) window.attachEvent("onload", sfHover);
 //--><!]]></script>
-<script type='text/javascript' src='/wp-content/themes/sbekarak/jquery.elementstack.js'></script> 
 </head>
 
 <body>
