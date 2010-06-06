aaac3d7f0aa7c26ade969d44769016a687cdb366
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 16:43:01 +0200

    only display group_id = 1

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -8,10 +8,8 @@ get_header(); ?>
 
 	<div id="contentleft">
 	    
-	    <h1><?php echo get_option('wpsc_default_category'); ?></h1>
-	    
 	    <div class='wpsc_categories wpsc_category_grid'>
-			<?php wpsc_start_category_query(array('category_group'=> get_option('wpsc_default_category'), 'show_thumbnails'=> 1, 'image_size' => array('width'=>170, 'height' => 170))); ?>
+			<?php wpsc_start_category_query(array('category_group'=> "1", 'show_thumbnails'=> 1, 'image_size' => array('width'=>170, 'height' => 170))); ?>
 				<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item" title='<?php wpsc_print_category_name();?>'>
 					<?php wpsc_print_category_image(170, 170); ?>
 				</a>
