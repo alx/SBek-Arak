d4a80da6fa6170bc37fb1695e2292e0309b013ba
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 17:58:43 +0200

    change header menu

--- wp-content/themes/sbekarak/header.php
+++ wp-content/themes/sbekarak/header.php
@@ -50,7 +50,7 @@ foreach ($options as $value) {
 		<li><a href="/profil/" title="Profil">Profil</a></li>
 		<li><a href="/presse/" title="Presse">Presse</a></li>
 		<li><a href="/contact/" title="Contact">Contact</a></li>
-		<li><a href="/resellers/" title="Points de Vente">Points de Vente</a></li>
+		<li><a href="/sbekarak_catalog.pdf" title="Catalogue">Catalogue (pdf)</a></li>
 	</ul>
 
 </div>
