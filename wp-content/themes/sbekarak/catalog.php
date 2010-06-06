f0e786c6359caab53a6562fa4d582101c16d2151
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 16:40:51 +0200

    do not pring subcat

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: CatalogFR
+Template Name: Catalog
 */
 get_header(); ?>
 
@@ -15,7 +15,6 @@ get_header(); ?>
 				<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item" title='<?php wpsc_print_category_name();?>'>
 					<?php wpsc_print_category_image(170, 170); ?>
 				</a>
-				<?php wpsc_print_subcategory("", ""); ?>
 			<?php wpsc_end_category_query(); ?>
 			<div class='clear_category_group'></div>
 		</div>
