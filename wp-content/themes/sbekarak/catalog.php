7af03707968d92f0c0ded640800148e130d0ba48
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 17:43:07 +0200

    set height/width on catalog

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -10,17 +10,17 @@ get_header(); ?>
 	    
         <div class="wpsc_categories wpsc_category_grid">
             <a href="http://sbekarak.alexgirard.com/categories/au-quotidien/" class="wpsc_category_grid_item" title="Au Quotidien">
-                <img src="index.php?wpsc_request_image=true&amp;category_id=7&amp;width=170&amp;height=170" alt="Au Quotidien" title="Au Quotidien" class="wpsc_category_image"><br>
+                <img src="index.php?wpsc_request_image=true&amp;category_id=7&amp;width=170&amp;height=170" alt="Au Quotidien" title="Au Quotidien" class="wpsc_category_image" height="170px" width="170px"><br>
                 <span class="category_title">Au Quotidien</span>
             </a>
             
             <a href="http://sbekarak.alexgirard.com/categories/mini/" class="wpsc_category_grid_item" title="Mini">
-                <img src="index.php?wpsc_request_image=true&amp;category_id=9&amp;width=170&amp;height=170" alt="Mini" title="Mini" class="wpsc_category_image"><br>
+                <img src="index.php?wpsc_request_image=true&amp;category_id=9&amp;width=170&amp;height=170" alt="Mini" title="Mini" class="wpsc_category_image" height="170px" width="170px"><br>
                 <span class="category_title">Mini</span>
             </a>
             
             <a href="http://sbekarak.alexgirard.com/categories/soiree/" class="wpsc_category_grid_item" title="Soirée">
-                <img src="index.php?wpsc_request_image=true&amp;category_id=8&amp;width=170&amp;height=170" alt="Soirée" title="Soirée" class="wpsc_category_image"><br>
+                <img src="index.php?wpsc_request_image=true&amp;category_id=8&amp;width=170&amp;height=170" alt="Soirée" title="Soirée" class="wpsc_category_image" height="170px" width="170px"><br>
                 <span class="category_title">Soirée</span>
             </a>
 
