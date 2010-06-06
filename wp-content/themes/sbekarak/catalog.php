adab9c56a1441cf7f1fe4a9ca3cb05f1b82c8635
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 16:18:41 +0200

    display catalog

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -9,13 +9,9 @@ get_header(); ?>
 	<div id="contentleft">
 	    
 	    <div class='wpsc_categories wpsc_category_grid'>
-			<?php wpsc_start_category_query(array('category_group'=> get_option('wpsc_default_category'), 'show_thumbnails'=> 1)); ?>
-				<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item" title='<?php wpsc_print_category_name();?>'>
-					<?php wpsc_print_category_image(170, 170); ?>
-				</a>
-				<?php wpsc_print_subcategory("", ""); ?>
-			<?php wpsc_end_category_query(); ?>
-			<div class='clear_category_group'></div>
+			<?php echo wpsc_display_products_page(array('category_url_name'=>'au-quotidien')); ?>
+			<?php echo wpsc_display_products_page(array('category_url_name'=>'soiree')); ?>
+			<?php echo wpsc_display_products_page(array('category_url_name'=>'mini')); ?>
 		</div>
 		
 	</div>
