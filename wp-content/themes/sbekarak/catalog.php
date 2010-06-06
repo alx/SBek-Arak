3e30e28880caccd2f45a8cea2727deb8fc1fe95c
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 16:46:30 +0200

    print category name

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -11,8 +11,8 @@ get_header(); ?>
 	    <div class='wpsc_categories wpsc_category_grid'>
 			<?php wpsc_start_category_query(array('category_group'=> "1", 'show_thumbnails'=> 1, 'image_size' => array('width'=>170, 'height' => 170))); ?>
 				<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item" title='<?php wpsc_print_category_name();?>'>
-					<?php wpsc_print_category_image(170, 170); ?>
-					<?php wpsc_print_category_name(); ?>
+					<?php wpsc_print_category_image(170, 170); ?><br>
+					<span class="category_title"><?php wpsc_print_category_name(); ?></span>
 				</a>
 				
 			<?php wpsc_end_category_query(); ?>
