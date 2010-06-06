5a6f7ae2d5c170e4ef536d1bcc687e2b09a743cc
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 18:00:54 +0200

    update css

--- wp-content/themes/sbekarak/catalog.php
+++ wp-content/themes/sbekarak/catalog.php
@@ -8,7 +8,7 @@ get_header(); ?>
 
 	<div id="contentleft">
 	    
-        <div class="wpsc_categories wpsc_category_grid">
+        <div class="wpsc_categories wpsc_category_grid" style="padding:100px 0px">
             <a href="http://sbekarak.alexgirard.com/categories/au-quotidien/" class="wpsc_category_grid_item" title="Au Quotidien">
                 <img src="index.php?wpsc_request_image=true&amp;category_id=7&amp;width=170&amp;height=170" alt="Au Quotidien" title="Au Quotidien" class="wpsc_category_image" height="170px" width="170px"><br>
                 <span class="category_title">Au Quotidien</span>
