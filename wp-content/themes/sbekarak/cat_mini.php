df279f958c7811c9589a24139c672070768b64a8
Alexandre Girard <alx.girard@gmail.com>
2010-06-06 17:50:33 +0200

    display sub-cat

--- wp-content/themes/sbekarak/cat_mini.php
+++ wp-content/themes/sbekarak/cat_mini.php
@@ -9,7 +9,7 @@ get_header(); ?>
 	<div id="contentleft">
 	    
 	    <div class='wpsc_categories wpsc_category_grid'>
-			<?php wpsc_start_category_query(array('category_parent' => 9, 'category_group'=> "all", 'show_thumbnails'=> 1, 'image_size' => array('width'=>170, 'height' => 170))); ?>
+			<?php wpsc_start_category_query(array('parent_category_id' => 9, 'show_thumbnails'=> 1, 'image_size' => array('width'=>170, 'height' => 170))); ?>
 				<a href="<?php wpsc_print_category_url();?>" class="wpsc_category_grid_item" title='<?php wpsc_print_category_name();?>'>
 					<?php wpsc_print_category_image(170, 170); ?><br>
 					<span class="category_title"><?php wpsc_print_category_name(); ?></span>
